869cb5ec52f5a696f0467573f8d93cee292880d0
Piotr M <malek.piotr96@gmail.com>
2020-03-18 00:45:13 +0100

    Zadanie wykonane, heron.js

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -1,11 +1,13 @@
 // boki trójkąta
 //a = 3;
-b = 4;
-c = 5;
-h = 7
+let b = 4;
+let c = 5;
+let h = 7;
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
 
-let a = () => "aaaa";
+let p = (b+c+h)/2;
+let syntax = p * (Math.sqrt((p-b)*(p-c)*(p-h)))
 
-console.log( a() );
+
+console.log("Pole trókąta o bokach " + b + ", " + c +" " + "oraz " + h + " wynosi " + Math.round(syntax*1000)/1000 + " jednostek kwadratowych. ");
