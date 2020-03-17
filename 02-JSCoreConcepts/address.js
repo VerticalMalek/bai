d8f056d27e0c6b5831c87d2944c69ad948942065
Piotr M <malek.piotr96@gmail.com>
2020-03-18 00:11:30 +0100

    Zadanie wykonane, address.js

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -1,13 +1,17 @@
-title = "dr";
-name = "Mateusz";
-surname = 'Kubicki';
-street = 'Długa 15';
-city = 'Kraków';
-zip = '30-781';
-country = 'Poland';
+let title = "dr";
+let name = "Mateusz";
+let surname = 'Kubicki';
+let street = 'Długa 15';
+let city = 'Kraków';
+let zip = '30-781';
+let country = 'Poland';
 
+console.log(title + " " + name + " " + surname+ "\n" + "ul. " + street);
+console.log(zip + " " + city);
+console.log(country.toUpperCase());
 
-/* 
+console.log(title + " " + name + " " + surname+ "\n" + "ul. " + street + "\n" + zip + " " + city + "\n" + country.toUpperCase());
+/*
 dr Mateusz Kubicki
 ul. Długa 15
 30-781 Kraków
