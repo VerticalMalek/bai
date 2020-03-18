af70fa458fd6fde3891f9de003c7ef429c6532e1
Piotr M <malek.piotr96@gmail.com>
2020-03-18 01:38:59 +0100

    Zadanie wykonane, cars.js

--- 02-JSCoreConcepts/cars.js
+++ 02-JSCoreConcepts/cars.js
@@ -1,10 +1,15 @@
 function isCar(car1, car2, car3) {
-    // return 
-}
+  if (car1 && car2 && car3) {
+    return true;
+  };
+  if (!car1 && !car2 && !car3) {
+    return true;
+  };
+return false;
+};
 
 console.log(isCar(true, true, true));
 console.log(isCar(false, false, false));
 console.log(isCar(true, true, false));
 console.log(isCar(false, false, true));
 console.log(isCar(true, false, true));
-
