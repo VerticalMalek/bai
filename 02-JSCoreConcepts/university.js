ae86fec69fa737554d331727ddf277d4a0d8b5e9
VerticalMalek <malek.piotr96@gmail.com>
2020-03-22 11:15:26 +0100

    Zadanie wykonane, university.js

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -1,35 +1,32 @@
-names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
+const names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
 
 class Students {
     constructor(names) {
-        // ...
+        this.names = names;
     }
 
     startWith(letter){
-        return // names (array) which starts with the letter
+        return new Students(this.names.filter(name => name.charAt(0).includes(letter)));
     }
 
     sort() {
-        return // names (array) in alphabetical order 
+        return new Students(this.names.sort());
     }
 
     get() {
-        return // names (array) in natural order
+        return new Students(this.names);
     }
 
     getFirst(n) {
-        return // get first 'n' names (array) 
+        return new Students(this.names.slice(0,n));
     }
 }
 
-students = new Students(names);
+const students = new Students(names);
 console.log(students.get());
 console.log(students.sort());
 console.log(students.startWith('P'));
 console.log(students.startWith('M').sort());
 console.log(students.sort().getFirst(4));
 console.log(students.startWith('M').sort().getFirst(2));
-console.log(students.get());
-
-
-
+//console.log(students.get());
